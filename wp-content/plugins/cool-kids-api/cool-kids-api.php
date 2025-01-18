d8e2652602ec453014b76162c316e6b4f4397f02
Rohit Vakhariya <rohit.vakhariya@gmail.com>
2025-01-18 17:09:56 +0530

    Added Basic authentication for API

--- wp-content/plugins/cool-kids-api/cool-kids-api.php
+++ wp-content/plugins/cool-kids-api/cool-kids-api.php
@@ -16,8 +16,7 @@ add_action('rest_api_init', function () {
         'methods' => 'POST',
         'callback' => 'assign_user_role',
         'permission_callback' => function () {
-        //return current_user_can('manage_options'); // Only admins can access
-        return true;
+        return current_user_can('manage_options'); // Only admins can access
         }
     ]);
 });
